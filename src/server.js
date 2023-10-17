dee56af039ebab320e30af6483883050a01d72e5
ferry <ferry.krist@gmail.com>
2023-10-17 15:09:21 +0700

    message broker producer

--- src/server.js
+++ src/server.js
@@ -25,82 +25,93 @@ const collaborations = require('./api/collaborations');
 const CollaborationsService = require('./services/postgres/CollaborationsService');
 const CollaborationsValidator = require('./validator/collaborations');
 
+const _exports = require('./api/exports');
+const ProducerService = require('./services/rabbitmq/ProducerService');
+const ExportsValidator = require('./validator/exports');
+
 const init = async () => {
-  const collaborationsService = new CollaborationsService();
-  const notesService = new NotesService(collaborationsService);
-  const usersService = new UsersService();
-  const authenticationsService = new AuthenticationsService();
+    const collaborationsService = new CollaborationsService();
+    const notesService = new NotesService(collaborationsService);
+    const usersService = new UsersService();
+    const authenticationsService = new AuthenticationsService();
 
-  const server = Hapi.server({
-    port: process.env.PORT,
-    host: process.env.HOST,
-    routes: {
-      cors: {
-        origin: ['*'],
-      },
-    },
-  });
+    const server = Hapi.server({
+        port: process.env.PORT,
+        host: process.env.HOST,
+        routes: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    });
 
-  // registrasi plugin eksternal
-  await server.register([
-    {
-      plugin: Jwt,
-    },
-  ]);
+    // registrasi plugin eksternal
+    await server.register([
+        {
+            plugin: Jwt,
+        },
+    ]);
 
-  // mendefinisikan strategy autentikasi jwt
-  server.auth.strategy('notesapp_jwt', 'jwt', {
-    keys: process.env.ACCESS_TOKEN_KEY,
-    verify: {
-      aud: false,
-      iss: false,
-      sub: false,
-      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
-    },
-    validate: (artifacts) => ({
-      isValid: true,
-      credentials: {
-        id: artifacts.decoded.payload.id,
-      },
-    }),
-  });
+    // mendefinisikan strategy autentikasi jwt
+    server.auth.strategy('notesapp_jwt', 'jwt', {
+        keys: process.env.ACCESS_TOKEN_KEY,
+        verify: {
+            aud: false,
+            iss: false,
+            sub: false,
+            maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+        },
+        validate: (artifacts) => ({
+            isValid: true,
+            credentials: {
+                id: artifacts.decoded.payload.id,
+            },
+        }),
+    });
 
-  await server.register([
-    {
-      plugin: notes,
-      options: {
-        service: notesService,
-        validator: NotesValidator,
-      },
-    },
-    {
-      plugin: users,
-      options: {
-        service: usersService,
-        validator: UsersValidator,
-      },
-    },
-    {
-      plugin: authentications,
-      options: {
-        authenticationsService,
-        usersService,
-        tokenManager: TokenManager,
-        validator: AuthenticationsValidator,
-      },
-    },
-    {
-      plugin: collaborations,
-      options: {
-        collaborationsService,
-        notesService,
-        validator: CollaborationsValidator,
-      },
-    },
-  ]);
+    await server.register([
+        {
+            plugin: notes,
+            options: {
+                service: notesService,
+                validator: NotesValidator,
+            },
+        },
+        {
+            plugin: users,
+            options: {
+                service: usersService,
+                validator: UsersValidator,
+            },
+        },
+        {
+            plugin: authentications,
+            options: {
+                authenticationsService,
+                usersService,
+                tokenManager: TokenManager,
+                validator: AuthenticationsValidator,
+            },
+        },
+        {
+            plugin: collaborations,
+            options: {
+                collaborationsService,
+                notesService,
+                validator: CollaborationsValidator,
+            },
+        },
+        {
+            plugin: _exports,
+            options: {
+                service: ProducerService,
+                validator: ExportsValidator,
+            },
+        },
+    ]);
 
-  await server.start();
-  console.log(`Server berjalan pada ${server.info.uri}`);
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
 init();
