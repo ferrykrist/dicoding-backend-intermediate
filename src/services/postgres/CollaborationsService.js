af5331f7a3df35c6cc69fa7c2ee311c1fca685b8
Ferry <ferry.krist@gmail.com>
2023-10-20 20:22:17 +0700

    server-side caching implemented

--- src/services/postgres/CollaborationsService.js
+++ src/services/postgres/CollaborationsService.js
@@ -3,51 +3,54 @@ const { nanoid } = require('nanoid');
 const InvariantError = require('../../exceptions/InvariantError');
 
 class CollaborationsService {
-  constructor() {
-    this._pool = new Pool();
-  }
-
-  async addCollaboration(noteId, userId) {
-    const id = `collab-${nanoid(16)}`;
-
-    const query = {
-      text: 'INSERT INTO collaborations VALUES($1, $2, $3) RETURNING id',
-      values: [id, noteId, userId],
-    };
-
-    const result = await this._pool.query(query);
-
-    if (!result.rows.length) {
-      throw new InvariantError('Kolaborasi gagal ditambahkan');
+    constructor(cacheService) {
+        this._pool = new Pool();
+        this._cacheService = cacheService;
     }
-    return result.rows[0].id;
-  }
 
-  async deleteCollaboration(noteId, userId) {
-    const query = {
-      text: 'DELETE FROM collaborations WHERE note_id = $1 AND user_id = $2 RETURNING id',
-      values: [noteId, userId],
-    };
+    async addCollaboration(noteId, userId) {
+        const id = `collab-${nanoid(16)}`;
 
-    const result = await this._pool.query(query);
+        const query = {
+            text: 'INSERT INTO collaborations VALUES($1, $2, $3) RETURNING id',
+            values: [id, noteId, userId],
+        };
 
-    if (!result.rows.length) {
-      throw new InvariantError('Kolaborasi gagal dihapus');
+        const result = await this._pool.query(query);
+
+        if (!result.rows.length) {
+            throw new InvariantError('Kolaborasi gagal ditambahkan');
+        }
+        await this._cacheService.delete(`notes:${userId}`);
+        return result.rows[0].id;
     }
-  }
 
-  async verifyCollaborator(noteId, userId) {
-    const query = {
-      text: 'SELECT * FROM collaborations WHERE note_id = $1 AND user_id = $2',
-      values: [noteId, userId],
-    };
+    async deleteCollaboration(noteId, userId) {
+        const query = {
+            text: 'DELETE FROM collaborations WHERE note_id = $1 AND user_id = $2 RETURNING id',
+            values: [noteId, userId],
+        };
 
-    const result = await this._pool.query(query);
+        const result = await this._pool.query(query);
 
-    if (!result.rows.length) {
-      throw new InvariantError('Kolaborasi gagal diverifikasi');
+        if (!result.rows.length) {
+            throw new InvariantError('Kolaborasi gagal dihapus');
+        }
+        await this._cacheService.delete(`notes:${userId}`);
     }
-  }
+
+    async verifyCollaborator(noteId, userId) {
+        const query = {
+            text: 'SELECT * FROM collaborations WHERE note_id = $1 AND user_id = $2',
+            values: [noteId, userId],
+        };
+
+        const result = await this._pool.query(query);
+
+        if (!result.rows.length) {
+            throw new InvariantError('Kolaborasi gagal diverifikasi');
+        }
+    }
 }
 
 module.exports = CollaborationsService;
